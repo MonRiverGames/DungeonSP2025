3d3dc9aadabaf56266f577553a13eb46e5e5ec64
htcole <56606919+htcole@users.noreply.github.com>
2025-03-26 13:23:17 -0400

    Final touches
    After approving two pull requests, updating this with one with the other pieces of the puzzles went less than ideal. More fixes were necessary than I would have preferred. Reminder: keep big things modular. Inventory has its own cs file so it shouldn't be a list or dictionary in program.cs or room.cs. Anything big should get its own .cs file that we reference to in other files.

--- Room.cs
+++ Room.cs
@@ -1,45 +1,21 @@
-﻿using System.ComponentModel.Design;
+﻿using System;
+using System.Collections.Generic;
 
-namespace DungeonGame;
-// Room Class
-class Room
+namespace DungeonGame
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public Dictionary<string, Room> Exits { get; set; }
-
-    public Room(string name, string description)
+    public class Room
     {
-        Name = name;
-        Description = description;
-        Exits = new Dictionary<string, Room>();
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Dictionary<string, Room> Exits { get; private set; }
+        public List<string> Items { get; private set; }
 
-    
-
-        Room Caverns = new Room("Creepy Caverns", "A dark cave with dripping stalactites.");
-
+        public Room(string name, string description)
         {
-            Exits["forward"]= Caverns;
-      
-                Console.WriteLine("You are in: Creepy Caverns");
-                Console.WriteLine(Caverns.Description);
-
-            string direction = "forward";
-
-            if (direction == "forward") 
-            {
-                Console.WriteLine("You have found a sword!");
-            }
-            else
-            {
-                Console.WriteLine("Not allowed to go that way.");
-            }
-
-
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            Exits = new Dictionary<string, Room>();
+            Items = new List<string>();
         }
-
-
-
-
     }
 }
