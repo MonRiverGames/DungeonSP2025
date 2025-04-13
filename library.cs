f406d02c420a9880a843a748f0e7994d79448491
hayden-scher <haydenscher@gmail.com>
2025-04-13 19:46:25 -0400

    Create library.cs

---
+++ library.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace DungeonGame
+
+     
+    switch (library)
+{ 
+    case 1:
+    Console.WriteLine("You have entered the library");
+    break;
+    case 2:
+    Console.WriteLine("You have entered the library again with Rubies in your inventory");
+
+
+}
