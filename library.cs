3d5b353122bf671f0fd188d91b484ff5e302d808
hayden-scher <haydenscher@gmail.com>
2025-04-13 19:56:02 -0400

    Update library.cs

--- library.cs
+++ library.cs
@@ -12,6 +12,31 @@ namespace DungeonGame
     break;
     case 2:
     Console.WriteLine("You have entered the library again with Rubies in your inventory");
+    break;
+    case 3:
+    Console.WriteLine("Well Well looks like you found gold and have entered where one must read");
+    break;
+    case 4:
+    Console.WriteLine("Your a good miner and found Emeralds. You have enetered the library");
+    break;
+    case 5:
+    Console.WriteLine("OOOO shiny blue diamonds. Chicken Jockeyyyy. You have re entered the lobrary");
+    break;
+    case 6:
+    Console.WriteLine("That Death Posion you foudn looks spooky. You have once again entered the library");
+    break;
+    case 7:
+    Console.WriteLine("Dont burn yourseld with that fire spell. You have enetered the library");
+    break;
+    case 8:
+    Console.WriteLine("You found the Regan spell. You have enetered the library");
+    break;
+    case 9:
+    Console.WriteLine("What are you Jack Frost with that ice spell. You have enetered the library");
+    break;
+    case 10:
+    Console.WriteLine("You found a copper key. You have enetered the library again");
+    break;
 
 
 }
